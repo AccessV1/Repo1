e16df278a8a951bcc142b502b03e5b68375dcffd
bryanpow <ramosbusiness544@gmail.com>
2024-10-01 21:07:17 -0400

    global styles

--- frontend/app/screens/LoginScreen.tsx
+++ frontend/app/screens/LoginScreen.tsx
@@ -1,9 +1,15 @@
-import {View, Text, Button} from 'react-native'
+import {View, Text, Button, Image, StyleSheet} from 'react-native'
 
 function LoginScreen() {
   return (
-   <View><Text>Login</Text></View>
+   <View>
+    <Image source={require('../assets/openlogo1.png')}/>
+   </View>
   )
 }
 
 export default LoginScreen
+
+
+//styling
+
