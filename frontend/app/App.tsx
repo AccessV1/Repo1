87c1872b9a30978ce829d64bb7e6cd343012c7f4
bryanpow <ramosbusiness544@gmail.com>
2024-10-02 22:40:10 -0400

    login ui progress

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, Text } from 'react-native';
 import { RootLayout } from './layouts/RootLayout';
-import LoginScreen from './screens/LoginScreen';
+
 
 export default function App() {
   return (
     <RootLayout style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-      <LoginScreen />
+      Access App
     </RootLayout>
   );
 }
