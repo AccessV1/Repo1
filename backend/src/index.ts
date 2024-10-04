4a709ff4e8655189f4789631e53f89e23f87fcdd
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-10-04 00:13:20 -0400

    basic api auth routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { errorHandlingMiddleware } from "./middleware/errorHandlingMiddleware";
+import apiRouter from "./api";
 
 const app: Application = express();
 const port: number = 3000;
@@ -14,7 +15,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("server is working");
 });
 
-
+app.use("/api", apiRouter);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}...`);
