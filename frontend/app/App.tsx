df5f30e3d175409e964457f9703733417d46b34c
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-10-05 22:41:34 -0700

    made name input screen && refarcted subpar code

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -2,12 +2,13 @@ import { View, Text } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import { RootLayout } from './layouts/RootLayout';
 import '../global.css';
-
+import { NameInputScreen } from './screens/NameInputScreen';
 export default function App() {
   return (
     <RootLayout style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
       {/* <Text className="text-5xl text-green-100">The Access App</Text> */}
-      <LoginScreen />
+      {/* <LoginScreen /> */}
+      <NameInputScreen />
     </RootLayout>
   );
 }
---
+++ frontend/app/screens/NameInputScreen.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import {
+  TextInput,
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import ConditionalButton from 'app/components/ui/ConditionalButton';
+
+/***
+ * -ALERT change this to use formData value of name from props when the parent component is made
+ */
+export const NameInputScreen = () => {
+    const [name, setName] = useState<string>('');
+    
+
+  const isButtonDisabled = name.trim() === '';
+
+  const handlePress = () => {
+    if (!isButtonDisabled) {
+      console.log(`User's name is: ${name}`);
+      // Handle form submission logic
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className="mx-10 mt-[25vh] w-[85vw] flex-1 items-start gap-[20] bg-white">
+        <Text className="self-start text-left text-4xl font-semibold">What</Text>
+        <Text className="self-start text-left text-4xl font-semibold">should we call you?</Text>
+
+        <TextInput
+          keyboardType="default"
+          className="mt-5 h-12 w-[100%] rounded-lg bg-gray-100 px-4 text-lg"
+          placeholder="Enter your name"
+          placeholderTextColor="#C4C4C4"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+
+        <ConditionalButton disabled={isButtonDisabled} title="Let's Begin" className="w-full absolute bottom-0" />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
