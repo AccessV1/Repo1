12112f31ae266a96f5e6c1d92550ca952773ffb2
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-10-12 01:42:42 -0700

    converted root layout styles to tailwind

--- frontend/app/layouts/RootLayout.tsx
+++ frontend/app/layouts/RootLayout.tsx
@@ -8,7 +8,7 @@ interface RootLayoutProps {
 
 export const RootLayout: React.FC<RootLayoutProps> = ({ children, style }): React.ReactNode => {
   return (
-    <SafeAreaView  style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+    <SafeAreaView className="h-full items-center justify-center">
       <View style={style}>{children}</View>
     </SafeAreaView>
   );
